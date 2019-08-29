9497749ab4a7f26f89c73eac92b59e9bdf144482
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 12:56:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
-  def initialize(discount = "false")
+  def initialize(discount = 0)
     @total = 0 
-    @discount 
+    @discount = discount
   end 
   
   def discount 
-    @discount = 20
+    @discount
   end 
   
   attr_accessor :total 
