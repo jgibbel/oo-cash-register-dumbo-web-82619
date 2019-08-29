f581ddf913d30ef7341b2e70afacec19dc8bda3d
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 12:55:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
-
+class CashRegister
+  def initialize(discount = "false")
+    @total = 0 
+    @discount 
+  end 
+  
+  def discount 
+    @discount = 20
+  end 
+  
+  attr_accessor :total 
+  
+  def add_item(name, price, quantity=1)
+    @total += price*quantity
+  end
+  
+  def apply_discount
+    if @discount
+    @total = @total * (100-@discount) / 100 
+  end
+  end 
+end 
